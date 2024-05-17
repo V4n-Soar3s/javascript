dc55b25d1ba5abecc08ffb1341867a1c92cdd0e3
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-05-17 15:04:47 -0300

    aprendendo_Js

---
+++ aula4/comentarios.js
@@ -0,0 +1,12 @@
+// Podemos escrever um comentário em JS utilizando duas barras (anotações), é ignorado pelo vscode o // é usado para comentario de linha
+console.log('Olá mundo'); //aqui tem outro comentario 
+console.log('Linha 1 ');
+//console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+//outro trecho enginne ou motor é o node utiliza o mesmo motor do Google CHROME que é a V8
+//Comentário
+/*
+Um bloco de código
+Linha de comentário
+*/
