b040ffedef7c3565686e84165a2651cfc4d4ee95
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-05-17 18:34:19 -0300

    aulas_const

---
+++ aula7/const001.js
@@ -0,0 +1,16 @@
+// Constantes - Aula 07 (Variável constante é uma variável que nao muda o seu valor)
+
+/*
+ * Não podemos criar constantes com palavras reservadas
+ * Constantes precisam ter nomes significativos
+ * Não podemos começar o nome de uma constante com um nº
+ * Não podem conter espaços ou traços
+ * Utilizamos camelCase
+ * São case-sensitive
+ * Não podemos modificar o valor de uma constante
+ * Não podemos apenas declarar uma constante, precisamos declarar e definir ela. 
+ * NÃO UTILIZE VAR, UTILIZE CONST.
+ */
+
+const nome = 'Vanessa';
+console.log(nome);
---
+++ aula7/const002.js
@@ -0,0 +1,10 @@
+/*Podemos pegar o valor de uma variavel ou de uma constante e atribuir ao valor de outra variavel ou outra constante, vide exemplo abaixo:*/
+//  Operadores aritméticos mais utilizados: + - * / 
+
+const primeiroNumero = 5;
+const segundoNumero= 10;
+const resultado = primeiroNumero * segundoNumero; 
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+console.log(resultadoTriplicado);
+
