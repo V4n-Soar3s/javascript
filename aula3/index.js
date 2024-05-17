414503745beeea44621c712970d96948f1a71a71
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-05-17 10:12:59 -0300

    novo_conteudo

---
+++ aula3/index.js
@@ -0,0 +1,9 @@
+//console.log( ); - sempre que quisermos debugar o código, exibir alguma coisa seja de uma variável ou exibir um valor literal que utilizamos, devemos utilizar a função console.log( ); a mesma só é exibida no console não é apresentada para o usuário
+
+//Strings = Texto (Para declarar que algo é uma string precisamos colocar entre aspas ou crases)
+console.log("Vanessa 'Soares' ");/*Se eu precisar colocar meu texto dentro de aspas simples é necessário alterar a aspas simples da função para aspas duplas e envolver o meu texto com aspas a ser exibido entre aspas simples*/
+console.log('Vanessa "Soares"');/*O mesmo ocorre neste caso se eu quiser envolver o texto de dentro do conteúdo com aspas duplas preciso envolver a descricção com aspas simples*/
+console.log(`Vanessa Soares`);/*Template Strings, conseguimos tanto utilizar aspas simples como aspas duplas, pois, o motor do JavaScript não confunde a abertura ou fechamento das tags */
+
+//Números = Number 
+console.log(35, 15.85, 'Vanessa Soares');//Para exibir nºs não é necessário colocar entre aspas ou crases. Não há separação sobre nº de ponto flutuante ou nº inteiro tudo é number em JavaScript. 
