d983d95497f6077cf0f7009b3de2dbfb74181b23
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-05-18 16:52:05 -0300

    number_aula07

--- aula7/const002.js
+++ aula7/const002.js
@@ -1,10 +1,17 @@
 /*Podemos pegar o valor de uma variavel ou de uma constante e atribuir ao valor de outra variavel ou outra constante, vide exemplo abaixo:*/
 //  Operadores aritméticos mais utilizados: + - * / 
 
-const primeiroNumero = 5;
-const segundoNumero= 10;
+const primeiroNumero = '5'; // number 
+const segundoNumero= 10; 
 const resultado = primeiroNumero * segundoNumero; 
 const resultadoDuplicado = resultado * 2;
 let resultadoTriplicado = resultado * 3;
-console.log(resultadoTriplicado);
+resultadoTriplicado = resultadoTriplicado + 5;
+console.log(typeof (primeiroNumero + segundoNumero));
 
+//para ter certeza do tipo de algum dado podemos utilizar o comando abaixo: 
+console.log(typeof primeiroNumero);//qual é o tipo da variavel primeiro numero? (essa é a pergunta que se faz quando utilizamos essa função)
+console.log(typeof segundoNumero);
+
+
+//O sinal de soma é utilizado para concatenar strings e para fazer somas
