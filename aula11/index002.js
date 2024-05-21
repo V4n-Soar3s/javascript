e672d7fb3505e1141f89e24cfdd5ef7f134990b7
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-05-21 20:32:52 -0300

    organizando arquivos e vendo sobre contadores

--- aula11/index002.js
+++ aula11/index002.js
@@ -17,6 +17,22 @@
 
 
 // operador de incremento = ++ | Pré-incremento (++contador) e pós-incremento (contador++)
-let contador = 1;
-console.log(++contador);//2
-console.log(++contador);//3
+//let contador = 1; // melhor maneira 
+//contador++;
+//console.log(contador);
+
+// se eu quiser fazer o incremento de mais de um valor seria desta forma: 
+
+const passo = 50; //criamos um steap (passo)
+let contador = 0;
+
+contador = contador + passo;
+console.log(contador);
+contador = contador + passo;
+console.log(contador);
+contador = contador + passo;
+console.log(contador);
+contador = contador + passo;
+console.log(contador);
+contador = contador + passo;
+console.log(contador);
