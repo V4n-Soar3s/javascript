701f5c569d1543302311e9e022d3c435391f0a32
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-05-20 21:16:06 -0300

    diferença entre let e var

---
+++ aula9/index.js
@@ -0,0 +1,8 @@
+//ECMAScript 2015 (ES6)= é uma padronização da linguagem Javascript criada pela empresa ECMA INTERNATIONAL = JavaScript
+
+//Diferença entre VAR e LET 
+
+var nome = 'Vanessa'; // uma das diferenças é que em var podemos redeclarar uma variável de mesmo nome e o ultimo valor sempre vai ser o atual a ser lido pelo método console
+var nome = 'Soares';// esse vai aparecer como nome, pois, é o ultimo valor declarado 
+
+console.log(nome);
---
+++ aula9/let.js
@@ -0,0 +1,4 @@
+let nome = 'Luiz'// no caso do let para alterar não podemos iniciar a variavel secundaria com let
+nome = 'Otávio';// ex variavel secundaria, sem colocar let na frente da redeclaração 
+
+console.log(nome);
