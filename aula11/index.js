40f2a14339e5c7af36e382089c3c6fe6dd4e1a98
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-05-20 23:46:28 -0300

    att

--- aula11/index.js
+++ aula11/index.js
@@ -20,4 +20,4 @@
 const num1 = 5;
 const num2 = 2;
 const num3 = 10;
-console.log((num1 + num2) * num3);// precedência de operadores utilizando parenteses 
+console.log((num1 + num2) * num3);// precedência de operadores  
