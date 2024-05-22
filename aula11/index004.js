1b902bbd09b3e01c96d13ac253c69498b2c9cf88
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-05-21 21:06:20 -0300

    update aula 11

---
+++ aula11/index004.js
@@ -0,0 +1,26 @@
+/**
+ * incremento = ++
+ * decremento = --
+ */
+
+//Como realizar uma contagem de forma mais simplificado
+
+// Operadores de atribuição 
+// **= o valor que eu quero 
+
+let contador = 2;
+contador **=10;// contador = contador * 2 lê se dessa forma 
+console.log(contador);
+
+//NaN = not a number -> isso significa que a conta deu algum erro, que algum tipo de dado nao bateu na conta
+
+/* Como converter uma string em um number 
+
+const num1 = 10;
+const num2 = parseInt( '5' ); -> convertemos a string para um nº inteiro 
+console.log(num1 * num2);
+console.log(typeof num2);
+
+parseInt = conversão para nº inteiro, parseFloat = conversao para nº flutante (casas decimais) e number = converte para nº e se vira sozinho pra entender se é um numero inteiro ou flutuante ou se nao pode ser convertido 
+
+*/
