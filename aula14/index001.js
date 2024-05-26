9080526a1664ef56d043ff5f9526a6acf3de29eb
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-05-26 18:31:36 -0300

    strings

---
+++ aula14/index001.js
@@ -0,0 +1,5 @@
+//Strings são indexadas, cada caractere tem um indice esse indice começa de 01234567
+//               01234567   
+let umaString = "Um texto";
+
+console.log(umaString.concat(' ', 'em', ' ', 'um'));// caso eu queira uma concatenação 
