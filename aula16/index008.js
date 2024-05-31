de7f2774330be53ee980d5cb15dd3cdabd6cce56
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-05-31 18:42:10 -0300

    math.round e math random

---
+++ aula16/index008.js
@@ -0,0 +1,4 @@
+// Também consigo arredondar se necessário o random entre 10 e 5, exemplo: 
+
+const aleatorio = Math.round(Math.random( ) * (10 - 5) + 5);// adicionamos os parenteses e o Math.round antes da declaração fazendo que a mesma vire um número inteiro mais proximo 
+console.log(aleatorio);
