450bd755eb1867f0ff7b00ac930df56ec5a8f449
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-05-31 18:32:53 -0300

    Math.random

---
+++ aula16/index006.js
@@ -0,0 +1,6 @@
+//Objeto Math - Math.random p/ gerar números aleatórios entre 0 e 1 com varias casas decimais (o 1 nunca vai ser incluido) e cada vez que for executado irá gerar um número aleatório diferente
+
+
+const aleatório = Math.random( );// defino uma constante com uma função como resultado no caso random 
+
+console.log(aleatório);// e aqui irá retornar o número aleatório que foi executado na função acima
