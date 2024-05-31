d0e0ddda336ca2431f59d572dce977ec25c0d481
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-05-31 11:53:23 -0300

    metodos de number p/ strings

---
+++ aula15/index001.js
@@ -0,0 +1,7 @@
+//Numbers
+
+let num1 = 1;//definimos o primeiro número 
+let num2 = 2.5; //definimos o segundo número
+
+console.log(num1.toString() + num2);// p/ converter o nº para string temporariamente podemos usar toString desta forma 
+console.log(typeof num1);// nesse typeOf é apresentado ainda a resposta como num1 como um number porque a identidade inicial dele não foi alterada, apenas dentro da função foi moficado para que o mesmo pudesse executar uma operação e concatenar os valores
---
+++ aula15/index002.js
@@ -0,0 +1,10 @@
+//Caso eu realmente quisesse alterar o meu number para uma string p/ sempre faria desta forma 
+
+let num1 = 1;// number
+let num2 = 2.5;// number
+
+num1 = num1.toString( ); // para definir que um dos meus números realmente foi convertido para uma string para sempre realizo a alteração desta forma 
+console.log(typeof num1);// aqui solicito que seja identificado o tipo após a alteração da variável num1 e o valor a ser apresentado é STRING 
+
+
+
