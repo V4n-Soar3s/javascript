0aa1d2d5e7df008975d8223845852cdd8b81d31f
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-05-31 19:03:18 -0300

    Math.pow (potenciação)

---
+++ aula16/index010.js
@@ -0,0 +1,8 @@
+//Potenciação 
+
+console.log(Math.pow(2, 10));// elevando 2 a 10
+
+// Mas também podemos fazer com operador que da o mesmo resultado
+
+console.log(2 ** 10);// que é mais facil de utilizar do que utilizar o objeto pow
+
