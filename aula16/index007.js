be002501ab2fbeb75f1b6dc50648c42858fe66a8
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-05-31 18:36:59 -0300

    math.random entre 10 e 5

---
+++ aula16/index007.js
@@ -0,0 +1,4 @@
+// Posso também utilizar o random para colocar parametros de entre tal numero a tal numero, exemplo entre 10 e 5:
+
+const aleatorio = Math.random( ) * (10 - 5) + 5;// aqui vou deixar o meu numero aleatorio entre 10 e 5
+console.log(aleatorio);
