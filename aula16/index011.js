dc7d0b2bf30345452f2eed4a93c302cb84c98a16
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-05-31 19:35:37 -0300

    infinity

--- aula16/index011.js
+++ aula16/index011.js
@@ -1,5 +1,8 @@
 // Como pegar a raiz quadrada de um número: 
 
-let num1 = 12;
+let num1 = 16;
 
 console.log(num1 ** (1/2));// o nº que eu quero a raiz quadrada elevado por 2
+//posso fazer tbm da maneira direta que é mais facil, exemplo:
+
+console.log(num1 ** 0.5);
---
+++ aula16/index012.js
@@ -0,0 +1,3 @@
+//Em javascript é possível dividir um número por 0, vide exemplo:
+
+console.log(!!(100 / 0));// isso nao da erro e é avaliado como verdadeiro e retorna o valor infinity, porque quanto mais nos aproximamos do 0 absoluto a gente aumenta mais o nosso valor e em algum momento ele irá chegar no infinity 
