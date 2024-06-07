7764a869f29113ee88e8310c162472804da5fc93
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-06-06 21:07:27 -0300

    funcao_push

--- aula18/index004.js
+++ aula18/index004.js
@@ -1,5 +1,13 @@
 //caso eu queira saber o tamanho do meu array:
 
-const alunos = ['Luiz', 'Maria', 'João'];
+const alunos = ['Luiz', 'Maria', 'João'];//declarei o array 
 
-console.log(alunos);
+alunos[alunos.length] = 'Luiza'; //adicionei um elemento ao loway após o último 4
+alunos[alunos.length] = 'Marcos'; //adicionei um elemento ao loway após o último 5
+alunos[alunos.length] = 'Luana'; //adicionei um elemento ao loway após o último 6
+alunos[alunos.length] = 'Fábio'; //adicionei um elemento ao loway após o último 7
+alunos[alunos.length] = 'Leticia'; //adicionei um elemento ao loway após o último 8
+
+console.log(alunos.length);// pedi para ser exibido a quantidade de elementos indexados do meu array
+console.log(alunos); // solicitei que apareça a lista no console
+console.log(alunos[7]);// chamei apenas o elemento 7 após adicionar mais elementos ao array 
---
+++ aula18/index005.js
@@ -0,0 +1,7 @@
+//push = função para adicionar o elemento ao final do array 
+//               0       1        2    
+const alunos = ['Luiz', 'Maria', 'João'];
+
+alunos.push('Luiza');// solicito que seja adicionado um elemento no final do meu array
+alunos.push('Fábio'); // solicito que seja adicionado um elemento no final do meu array
+console.log(alunos);
