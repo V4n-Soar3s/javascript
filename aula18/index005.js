b92ccc51123b824e3e22eb9f53f098ba1c25a73e
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-06-06 21:16:23 -0300

    push e unshift

--- aula18/index005.js
+++ aula18/index005.js
@@ -1,7 +1,12 @@
 //push = função para adicionar o elemento ao final do array 
+//unshift = função para adicionar o elemento no ínicio do array e mudar a posição dos demais elementos existentes no array
+
 //               0       1        2    
 const alunos = ['Luiz', 'Maria', 'João'];
 
+alunos.unshift('Mariana');//adicionei um elemento no inicio do array 
+alunos.unshift('Carlos');//adicionei um elemento no inicio do array
 alunos.push('Luiza');// solicito que seja adicionado um elemento no final do meu array
 alunos.push('Fábio'); // solicito que seja adicionado um elemento no final do meu array
+
 console.log(alunos);
