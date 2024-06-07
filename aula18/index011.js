bc7fc5bec256e837a6bdd4692c865f688019df35
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-06-06 21:47:48 -0300

    instanceof_array

--- aula18/index011.js
+++ aula18/index011.js
@@ -2,5 +2,7 @@
 
 const alunos = ['Luiz', 'Maria', 'Joao'];
 
-console.log(typeof alunos);
+console.log(typeof alunos);// perguntando o tipo de valor que estou trabalhando 
+console.log(alunos instanceof Array);//aqui estou perguntando se alunos é uma instância de array se retornar true é array false não é array 
+
 
