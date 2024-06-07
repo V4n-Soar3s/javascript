5e538154a0ec457b8c337246f97c66d84714fc53
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-06-06 21:25:02 -0300

    pop_para_remover_elementos

---
+++ aula18/index006.js
@@ -0,0 +1,7 @@
+//Também podemos remover elementos do array utilizando pop, vide exemplo: 
+//               0       1        2
+const alunos = ['Luiz', 'Maria', 'João'];
+
+const removido = alunos.pop();//estou removendo o elemento do final do array, nesse caso foi tirado o João e podemos salvar o elemento que estamos removendo do array em uma variavel.
+console.log(removido);//aqui vai apresentar quem foi removido
+console.log(alunos);// aqui aparece a lista sem a pessoa que foi retirada
