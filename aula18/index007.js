bccfc4b235fcc176b8f87b3eebeb4dcc539b5689
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-06-06 21:30:00 -0300

    shift_para_remover_do_inicio

---
+++ aula18/index007.js
@@ -0,0 +1,7 @@
+//Também há a função shift que remove do começo do array, exemplo:
+
+const alunos = ['Luiz', 'Ana', 'Maria'];
+
+const removido = alunos.shift( );//removi um elemento do começo
+console.log(removido); //criei uma variavel para salvar o elemento removido 
+console.log(alunos); // solicitei que seja exibida a minha lista após a remoção do elemento 
