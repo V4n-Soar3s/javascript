8a5210b208571374475081825e6b831db6f44017
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-06-07 11:17:34 -0300

    funções

---
+++ aula19/index001.js
@@ -0,0 +1,8 @@
+//Funções (Básico) = funções executam ações, para criar ma função em javascript utilizamos a palavra function, vide exemplo: 
+
+
+function saudacao( ) {
+    console.log('Bom dia!')//tudo o que esta dentro da função esta protegido e pertence a ela 
+}//criamos uma função e demos um nome a ela, sempre que for criada uma função é necessário o parentes e as os colchetes para criarmos o corpo dela 
+
+saudacao();// chamei a função
