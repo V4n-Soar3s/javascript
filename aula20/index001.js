f7b80d4f497637f43977c0404cc471fb01b57906
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-06-07 21:31:46 -0300

    OBJETOS_FUNCTION

---
+++ aula20/index001.js
@@ -0,0 +1,24 @@
+//Objetos (Básico) - para criar um objeto são utilizadas as chaves {}
+
+//objeto literal
+const pessoa1 = { //criei um objeto 
+    nome:'Vanessa', // sempre no final das declarações dentro de um objeto colocar vírgula 
+    sobrenome:'Soares',
+    idade: 28
+};
+
+const pessoa2 = { //criei o segundo objeto
+    nome:'Bruna',
+    sobrenome:'Oliveira',
+    idade: 29
+};
+
+const pessoa3 = { //criei o terceiro objeto 
+    nome:'Joao',
+    sobrenome:'Almeida',
+    idade: 17
+};
+
+console.log(pessoa1.nome);// para exibir a informação selecionada sobre o objeto 1
+console.log(pessoa1.sobrenome);
+console.log(pessoa1.idade);
---
+++ aula20/index002.js
@@ -0,0 +1,16 @@
+//objetos  com function 
+
+
+//Essa função é chamada de factory pq ela retorna//cria objetos, ela é uma fábrica de objetos
+function criaPessoa (nome, sobrenome, idade) {
+    return {nome, sobrenome, idade};// quando o nome dos valores dentro do return forem os mesmos podemos deixar tudo numa linha só que a função já entende que o valor esta atrelado a aquele parametro 
+}
+
+const pessoa1 = criaPessoa('Vanessa', 'Soares', 28);// criei uma variavel para enviar argumentos para os parametros da função 
+const pessoa2 = criaPessoa('Luna', 'Gonçalves', 32);
+const pessoa3 = criaPessoa('Bruna', 'Oliveira', 29);
+const pessoa4 = criaPessoa('Vitor', 'Emanuel', 23);
+const pessoa5 = criaPessoa('Izabel', 'Aparecida', 15);
+
+console.log(pessoa1.nome, pessoa1.sobrenome);
+console.log(pessoa2.nome, pessoa2.idade);
