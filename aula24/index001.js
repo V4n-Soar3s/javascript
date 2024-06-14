53c80567a404c0e3073eeda388e2ca91cdf19128
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-06-14 17:57:02 -0300

    OPERADORES LOGICOS

---
+++ aula24/index001.js
@@ -0,0 +1,22 @@
+/*
+
+Operadores Lógicos 
+&& -> AND -> E - Todas as expressões precisam ser verdadeiras para retornar true quando utilizamos and
+|| - OR -> OU 
+! -> NOT -> NÃO
+
+
+Eles sao utilizados para checar mais de uma comparação 
+*/
+
+// && - AND - E 
+const expressaoAnd = true && true && false && true; // se uma das expressões for falsa ela retorna como false
+console.log(expressaoAnd);
+
+//|| - OR - OU
+
+const expressaoOr = false || false || true || false || false; // se uma das condições for verdadeira ela retorna como true
+console.log(expressaoOr);
+
+
+
