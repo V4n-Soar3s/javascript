e818064755a28f47c7065748af621cb230025d02
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-06-14 22:46:16 -0300

    short-circuit

---
+++ aula25/index003.js
@@ -0,0 +1,4 @@
+//|| OR - qualquer uma delas ser verdadeira para ser true 
+
+console.log(0 || false || null || NaN ||`Luiz Otávio` || true);//sera apresentado a mensagem luiz otavio pois é a primeira mensagem verdadeira que o short-circuit encontra
+
