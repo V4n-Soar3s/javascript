b8b74920b9712694fab8788851565f2893df49c9
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-06-14 22:24:32 -0300

    short-circuit

---
+++ aula25/index001.js
@@ -0,0 +1,14 @@
+/*
+ && ->  todas as expressões precisam ser verdadeiras para que and retorne true
+ || ->  todas as expressões precisam ser false para que o OR retorne false 
+ */
+
+ console.log('Luiz Otávio' && 0 && 'Maria');//nesse caso ele retornara a informação false pois 0 é considerada uma expressão falsa
+
+ console.log(`Luiz Otávio` && true && `Maria`);//nesse caso ele retornara maria pois todas as expressões sao verdadeiras e ele ira apresentar o ultimo valor da expressao verdadeira que foi checada
+
+ //quais sao os valores que avaliam em falso no JS?
+
+
+
+
