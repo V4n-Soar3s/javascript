49ad95182aeebf2f1d005527a74066394a69f3e9
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-06-14 22:39:07 -0300

    && em funções

--- aula25/index001.js
+++ aula25/index001.js
@@ -8,6 +8,21 @@
  console.log(`Luiz Otávio` && true && `Maria`);//nesse caso ele retornara maria pois todas as expressões sao verdadeiras e ele ira apresentar o ultimo valor da expressao verdadeira que foi checada
 
  //quais sao os valores que avaliam em falso no JS?
+/*
+
+Falsy values
+
+False - literal
+0 - zero
+'' " " - aspas tanto simples, duplas ou crase sao avaliadas como false 
+null// undefined 
+NaN
+
+*/
+
+console.log('Luiz' && 'Maria');//os dois sao verdadeiros e retorna o ultimo valor que foi checado
+console.log('Luiz' && undefined && 'Maria');
+
 
 
 
---
+++ aula25/index002.js
@@ -0,0 +1,10 @@
+//&& em funções 
+
+
+function falaOi ( ) {
+    return 'Oi';
+}
+
+const vaiExecutar = false;
+
+console.log(vaiExecutar && falaOi());
