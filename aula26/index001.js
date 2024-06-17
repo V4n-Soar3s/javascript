f469e5b82d836fb37315aaec615294f75328c1a3
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-06-16 22:41:35 -0300

    if, else if e else

---
+++ aula26/index001.js
@@ -0,0 +1,25 @@
+//Estruturas condicionais if, else if e else, servem para desviar o fluxo da nossa aplicação quando necessário 
+
+//entre 0 - 11 === bom dia 
+//entre 12 - 17 === boa tarde 
+//entre 18 - 23 === boa noite
+
+//if pode ser usado sozinho com a condição dele, ou seja ele nao precisa do else if 
+// sempre que utilizo a palavra else, preciso de um if antes  
+// posso ter varios else if's na condição/checagem 
+// e eu só posso um else por último na condição ou na checagem 
+//podemos usar condições sem else if, utilizando apenas if e else
+
+
+const hora = 25;
+
+if (hora >=0 && hora <=11  ){//se a hora for menor do que 11 será apresentada a mensagem bom dia 
+    console.log('Bom dia');
+} else if (hora >=12 && hora <=17) {
+    console.log('Boa tarde');
+} else if (hora >=18 && hora <=23){
+    console.log('Boa noite');
+} else {
+    console.log('Olá');
+}
+
---
+++ aula26/index002.js
@@ -0,0 +1,9 @@
+//exemplo de outra condição 
+
+const tenhoGrana = true;
+
+if (tenhoGrana) {
+    console.log('Vou sair de casa');
+} else {
+    console.log('Vou ficar em casa');
+}
