5d7f4bd772ffd87082ce268a0932694c9c63003f
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-06-16 23:06:01 -0300

    estruturas de condições

---
+++ aula27/index001.js
@@ -0,0 +1,11 @@
+// if, else if e else 
+
+const numero = 8;
+
+if (numero >= 0 && numero <=5) {//se isso ocorrer faça isso
+    console.log('O número está entre 0 e 5.')
+} else {// se não faça isso daqui 
+    console.log('O número NÃO está entre 0 e 5.');
+}
+
+
