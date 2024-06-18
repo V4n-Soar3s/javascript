169465883bca79dd8d37c70bce1c3401b4eccb5e
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-06-18 00:02:14 -0300

    atualizado

--- aula28/assets/js/index.js
+++ aula28/assets/js/index.js
@@ -1,5 +1,17 @@
 //capturar evento de submit do formulario 
 
-const form = document.querySelector('#formulario');
+const form = document.querySelector('#formulario');//criei a variavel para chamar meu formulario
 
-form
+form.addEventListener('submit', function(evento){
+    evento.preventDefault();
+    console.log('Evento previnido');
+    setResultado('Olá Mundo!');
+});//criamos uma função para parar o evento no caso vamos escutar o submit, deve ser criada uma função, seja criando ou adicionando ela. 
+
+
+//como puxar o resultado
+
+function setResultado(msg){
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = `<p>${msg}</p>`;
+}// toda vez que for enviado será criado um paragrafo 
