bbe4809ff64f29f6242fbac6ee2c1f4b066c0113
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-06-25 21:22:38 -0300

    continuidade do exercicio de IMC

--- aula28/assets/js/index.js
+++ aula28/assets/js/index.js
@@ -2,16 +2,32 @@
 
 const form = document.querySelector('#formulario');//criei a variavel para chamar meu formulario
 
-form.addEventListener('submit', function(evento){
+form.addEventListener('submit', function(evento){//posso chamar de event ou e 
     evento.preventDefault();
-    console.log('Evento previnido');
-    setResultado('Olá Mundo!');
+    const inputPeso = e.target.querySelector('#ipeso');
+    const inputAltura = e.target.querySelector('#ialtura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    console.log(peso, altura)
 });//criamos uma função para parar o evento no caso vamos escutar o submit, deve ser criada uma função, seja criando ou adicionando ela. 
 
 
+function criaP(){//funcao que cria parágrafos
+    const p = document.createElement('p'); //estou criando um paragrafo
+    return p;
+}
+
 //como puxar o resultado
+
+
 
 function setResultado(msg){
     const resultado = document.querySelector('#resultado');
-    resultado.innerHTML = `<p>${msg}</p>`;
+    resultado.innerHTML = '';
+
+    const p = criaP();
 }// toda vez que for enviado será criado um paragrafo 
+
+
