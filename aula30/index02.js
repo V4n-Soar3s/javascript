882ef1f5ffda2580a50e0b8a071354976e66c0b4
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-07-02 21:59:20 -0300

    formas de date

---
+++ aula30/index02.js
@@ -0,0 +1,4 @@
+// Outras formas de enviar datas
+
+const data = new Date(2024, 7, 1, 21, 56, 30);// sempre numero sem 0 a esquerda ordem (ano, mes, dia, horas, minutos, segundos)
+console.log(data.toString());
